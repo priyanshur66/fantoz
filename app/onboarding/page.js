d5b84ddda2da98d74730ce406d228a8254d45532
akashbiswas0 <akash8860biswas@gmail.com>
2024-09-08 03:29:27 +0530

    fixed allignment

--- app/onboarding/page.js
+++ app/onboarding/page.js
@@ -3,19 +3,13 @@ export default function Onboarding(){
     return(
         <div className="pt-16">
            <section className=" text-white py-20">
-          <div className="container mx-auto px-6 flex flex-col md:flex-row items-center">
+          <div className="container mx-auto px-5 flex flex-col md:flex-row items-center">
             <div className="md:w-1/2 mb-10 md:mb-0 mx-20">
-              <h1 className="text-4xl md:text-6xl font-bold mb-6">
-              We are transforming fan loyalty with blockchain-powered rewards
-              </h1>
-              <p className="text-xl mb-8">
-              Unveiling Token-Gated Flash Drops, where fan tokens unlock exclusive, limited-time opportunities. 
-              </p>
-              <div className="flex mb-8 flex-col sm:flex-row justify-center space-y-4 sm:space-y-0 sm:space-x-4">
-                <Link href="/onboarding/clubsetup" className="bg-white text-black hover:text-white border border-black hover:border-white px-6 py-5 rounded-lg text-center font-semibold hover:bg-black transition duration-300">
+              <div className="flex mb-8 flex-col sm:flex-col justify-center space-y-4 sm:space-y-0 sm:space-x-4">
+                <Link href="/onboarding/clubsetup" className="bg-white text-black hover:text-white border border-black hover:border-white px-3 py-10 rounded-lg text-center font-semibold hover:bg-black transition duration-300 mb-7 text-2xl">
                   Club: Issue gated flash drops
                 </Link>
-                <Link href="/onboarding/fansetup" className="bg-white text-black hover:text-white border border-black  px-6 hover:border-white py-5 rounded-lg text-center font-semibold hover:bg-black transition duration-300">
+                <Link href="/onboarding/fansetup" className="bg-white text-black hover:text-white border border-black  px-3 hover:border-white py-10 rounded-lg text-center font-semibold hover:bg-black transition duration-300 text-2xl">
                   Fan: Participate in flash sales for your clubs
                 </Link>
               </div>
