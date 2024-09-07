3a5fd00c6d3faa27549b56cf14f02b1cc8a4d287
akashbiswas0 <akash8860biswas@gmail.com>
2024-09-07 18:31:10 +0530

    added Navbar component

---
+++ components/Navbar.jsx
@@ -0,0 +1,16 @@
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import Link from "next/link";
+export default function Navbar(){
+    return(
+        <div>
+            <header className="bg-neutral-300 py-4 px-6 flex justify-between items-center m-5 rounded-md">
+                <Link href="\landing">
+                <h1 className="text-4xl font-extrabold font-sans text-pink-700">Fantoz</h1>
+                </Link>
+                <div className="flex space-x-2">
+                    <ConnectButton />
+                </div>
+            </header>
+        </div>
+    )
+}
