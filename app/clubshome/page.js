fdba01d4edd4deeba1239a6d1e1d2fb2f82b33f9
akashbiswas0 <akash8860biswas@gmail.com>
2024-09-08 20:24:46 +0530

    bug fixes

--- app/clubshome/page.js
+++ app/clubshome/page.js
@@ -10,14 +10,13 @@ export default function ClubsHome() {
           <div className="container mx-auto px-6 flex flex-row md:flex-row items-center">
             <div className="md:w-full  md:mb-0 mx-10">
               <h1 className="text-4xl md:text-6xl font-bold mb-6">
-                We are transforming <mark className="bg-pink-500 rounded-xl">fan loyalty</mark> with blockchain-powered rewards
+                We are transforming <mark className="bg-pink-500 rounded-xl px-1">fan loyalty</mark> with blockchain-powered rewards
               </h1>
               <div className="flex flex-row sm:flex-row justify-center space-y-4 sm:space-y-0 sm:space-x-4">
                 <Link href="/dropslist"> <div className="bg-white text-black hover:text-white border border-black hover:border-white px-20 py-8 rounded-lg text-center text-3xl font-extrabold hover:bg-black transition duration-300">
                   List drops
                 </div>
                 </Link>
-
                 <Link href="/orderdetails"> <div className="bg-white text-black hover:text-white border border-black  px-20 hover:border-white text-3xl py-8 rounded-lg text-center font-extrabold hover:bg-black transition duration-300">
                   Manage Orders
                 </div>
@@ -26,7 +25,6 @@ export default function ClubsHome() {
             </div>
 
             <div className="w-full ml-1">
-              
               <Spline scene="https://prod.spline.design/9GAia2Fvu0HwOWrZ/scene.splinecode"
                className="rounded-2xl" width={700} height={600} />
             </div>
